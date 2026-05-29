7e97d0903f5c75c727e6d4c54505e39f8108c9e9
Harley <143757576+chaezuha@users.noreply.github.com>
2026-05-29 03:55:42 -0700

    Workout logging foundations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [session, setNewSession] = useState([]);
+  const [workout, setNewWorkout] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newWeight, setNewWeight] = useState("");
+  const [newSet, setNewSet] = useState("");
+  const [newRep, setNewRep] = useState("");
+  const [newNote, setNewNotes] = useState("");
+
+  const addWorkout = (event) => {
+    event.preventDefault();
+
+    const workoutObject = {
+      name: newName,
+      weight: newWeight,
+      sets: newSet,
+      reps: newRep,
+      notes: newNote,
+    };
+
+    setNewWorkout(workout.concat(workoutObject));
+    setNewName("");
+    setNewWeight("");
+    setNewSet("");
+    setNewRep("");
+    setNewNotes("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleWeightChange = (event) => {
+    setNewWeight(event.target.value);
+  };
+
+  const handleSetChange = (event) => {
+    setNewSet(event.target.value);
+  };
+
+  const handleRepChange = (event) => {
+    setNewRep(event.target.value);
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNotes(event.target.value);
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div>
+      <h1>Save today's workout</h1>
+      <ul>
+        {workout.map((w) => (
+          <ul key={w.name}>
+            <div>Workout: {w.name}</div>
+            <div>Weight: {w.weight}</div>
+            <div>Sets: {w.sets}</div>
+            <div>Notes: {w.notes}</div>
+          </ul>
+        ))}
+      </ul>
+      <form onSubmit={addWorkout}>
+        <div>
+          Workout Name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          Weight: <input value={newWeight} onChange={handleWeightChange} />
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+        <div>
+          Set Amount: <input value={newSet} onChange={handleSetChange} />
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+        <div>
+          Target Rep Amount: <input value={newRep} onChange={handleRepChange} />
         </div>
-      </section>
+        <div>
+          Notes: <input value={newNote} onChange={handleNoteChange} />
+        </div>
+        <div>
+          <button type="submit">Save Workout</button>
+        </div>
+      </form>
+      <div></div>
+      <p></p>
+    </div>
+  );
+};
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+export default App;
