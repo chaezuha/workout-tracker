85de4c413234f383f715248caa4f664710e72cf5
Harley <143757576+chaezuha@users.noreply.github.com>
2026-05-30 16:48:41 -0700

    Reworked drag loc + added edit button scaffold

--- frontend/src/Exercise/Exercise.jsx
+++ frontend/src/Exercise/Exercise.jsx
@@ -3,6 +3,20 @@ import "./Exercise.css";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export const Exercise = ({ id, name, weight, sets, reps, notes, onDelete }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id });
@@ -12,13 +26,13 @@ export const Exercise = ({ id, name, weight, sets, reps, notes, onDelete }) => {
     transform: CSS.Transform.toString(transform),
   };
   return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className="exercise"
-    >
+    <div ref={setNodeRef} style={style} {...attributes} className="exercise">
+      <div
+        {...listeners}
+        className="text-2xl px-2 cursor-grab active:cursor-grabbing select-none text-muted-foreground hover:text-foreground"
+      >
+        ⠿
+      </div>
       <div className="space-y-1">
         <div className="font-semibold">{name}</div>
         <div className="text-sm text-muted-foreground">Weight: {weight}</div>
@@ -36,14 +50,49 @@ export const Exercise = ({ id, name, weight, sets, reps, notes, onDelete }) => {
       >
         Reps
       </Button>
-      <Button
-        variant="outline"
-        size="sm"
-        onClick={() => {}}
-        onPointerDown={(e) => e.stopPropagation()}
-      >
-        Edit
-      </Button>
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-sm">
+            <DialogHeader>
+              <DialogTitle>Edit Mode</DialogTitle>
+              <DialogDescription>
+                Make changes to your exercise
+              </DialogDescription>
+            </DialogHeader>
+            <FieldGroup>
+              <Field>
+                <Label htmlFor="name-1">Name</Label>
+                <Input id="name-1" name="name" defaultValue={name} />
+              </Field>
+              <Field>
+                <Label htmlFor="weight-1">Weight</Label>
+                <Input id="weight-1" name="weight" defaultValue={weight} />
+              </Field>
+              <Field>
+                <Label htmlFor="set-1">Sets</Label>
+                <Input id="weight-1" name="sets" defaultValue={sets} />
+              </Field>
+              <Field>
+                <Label htmlFor="rep-1">Reps</Label>
+                <Input id="weight-1" name="reps" defaultValue={reps} />
+              </Field>
+              <Field>
+                <Label htmlFor="weight-1">Notes</Label>
+                <Input id="note-1" name="notes" defaultValue={notes} />
+              </Field>
+            </FieldGroup>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
       <Button
         variant="destructive"
         size="sm"
